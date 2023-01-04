24c613d221e8784479500f43ae343ff752728cc1
Jingyu Hou <118997149+JinnyH16@users.noreply.github.com>
2023-01-04 21:00:15 +0000

    Add files via upload

---
+++ A2/A2_SVM.py
@@ -0,0 +1,78 @@
+import pandas as pd
+import cv2
+from sklearn.metrics import accuracy_score
+from PIL import Image
+import numpy as np
+from timeit import default_timer as timer
+from sklearn.svm import SVC
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import GridSearchCV
+
+
+
+def loadImage(path):
+    img = Image.open(path)
+    img = cv2.cvtColor(np.asarray(img), cv2.COLOR_RGB2BGR)
+    median = cv2.resize(img, (45, 55))
+    img_resize = Image.fromarray(cv2.cvtColor(median, cv2.COLOR_BGR2RGB))
+    img = img_resize.convert("L")
+    data = img.getdata()
+    # data = np.array(data).reshape(img.size[1], img.size[0])/100
+    return data
+
+
+'''
+grid = {'C': [0.2, 0.5, 1, 5, 10], 'kernel': ['linear', 'rbf', 'poly', 'sigmoid']}
+search = GridSearchCV(svc, grid,
+                        scoring='accuracy',
+                        cv=5,
+                        )
+search.fit(x_train, y_train)
+model = search.best_estimator_
+y_pred=model.predict(x_test)
+print("SVM test Accuracy:", accuracy_score(y_test, y_pred))
+
+print(search.best_params_)
+'''
+
+def A2_SVM():
+    start = timer()
+
+    # Load training data
+    dataset = pd.read_csv('./Datasets/celeba/labels.csv', sep="\t")  # read csv file
+    dataset.loc[dataset['smiling'] == -1, 'smiling'] = 0
+    y_train = dataset['smiling']
+    img_name1 = dataset['img_name']
+
+    x_train = []
+    for name in img_name1:
+        img_path = './Datasets/celeba/img/' + name  # get path based on image name
+        img = loadImage(img_path)
+        x_train.append(img)  # add pic to x_train
+
+
+    # Load test data
+    data_test = pd.read_csv('./Datasets/celeba_test/labels.csv',
+                            sep="\t")  # read csv file
+    data_test.loc[data_test['smiling'] == -1, 'smiling'] = 0
+    y_test = data_test['smiling']
+    img_name2 = data_test['img_name']
+
+    x_test = []
+    for name in img_name2:
+        img_path = './Datasets/celeba_test/img/' + name  # get path based on image name
+        img = loadImage(img_path)
+        x_test.append(img)  # add pic to x_train
+
+    # apply standard scalar
+    sc = StandardScaler()
+    x_train = sc.fit_transform(x_train)
+    x_test = sc.transform(x_test)
+
+    # apply SVM
+    svc = SVC(kernel='rbf', C=1)
+
+    svc.fit(x_train, y_train)
+    y_pred = svc.predict(x_test)
+    print("SVM train Accuracy:", accuracy_score(y_train, y_pred=svc.predict(x_train)))
+    print("SVM test Accuracy:", accuracy_score(y_test, y_pred))
---
+++ A2/A2_logistic_regression.py
@@ -0,0 +1,121 @@
+import pandas as pd
+import cv2
+from sklearn.metrics import confusion_matrix, classification_report, accuracy_score
+from PIL import Image
+import numpy as np
+from timeit import default_timer as timer
+from sklearn.preprocessing import StandardScaler
+
+
+
+def loadImage(path):
+    img = Image.open(path)
+    img = cv2.cvtColor(np.asarray(img), cv2.COLOR_RGB2BGR)
+    median = cv2.resize(img, (45,55))
+    img_resize = Image.fromarray(cv2.cvtColor(median, cv2.COLOR_BGR2RGB))
+    img = img_resize.convert("L")
+    data = img.getdata()
+    # data = np.array(data).reshape(img.size[1], img.size[0])/100
+    return data
+
+
+def sigmoid(z):
+    sig = 1. / (1. + np.exp(-z))
+    return sig
+
+
+def logRegParamEstimates(xTrain, yTrain):
+    intercept = np.ones((np.array(xTrain).shape[0], 1))
+    xTrain = np.concatenate((intercept, xTrain), axis=1)
+    theta = np.zeros(np.array(xTrain).shape[1])
+    for i in range(900):
+        z = np.dot(xTrain, theta)
+        h = sigmoid(z)
+        lr = 0.01
+        gradient = np.dot(xTrain.T, (h - yTrain)) / (yTrain.shape[0] * np.log(2))
+        theta -= lr * gradient
+
+    return theta
+
+
+def logRegrNEWRegrPredict(xTrain, yTrain, xTest):
+    theta = logRegParamEstimates(xTrain, yTrain)
+    intercept = np.ones((np.array(xTest).shape[0], 1))
+    xTest = np.concatenate((intercept, xTest), axis=1)
+    sig = sigmoid(np.dot(xTest, theta))
+    y_pred1 = sig >= 0.5  # true or false assignment
+    return y_pred1
+
+
+def getK(y):  # intermediate function
+    x = 0
+    k = np.ones((np.array(y).shape[0]))
+    for i in y:
+        if i == True:
+            k[x] = 1
+        elif i == False:
+            k[x] = 0
+        x = x + 1
+
+    return k
+
+
+def getMSE(k,y): # Mean square error
+
+    p = 0
+    for j in range(len(k)):
+        a = np.square(k[j] - y[j])
+        p = p + a
+
+    MSE = p / len(k)
+
+    return MSE
+
+
+def A2_logisticregression():
+    start = timer()
+    # Load training data
+    dataset = pd.read_csv('./Datasets/celeba/labels.csv', sep="\t")  # read csv file
+    dataset.loc[dataset['smiling'] == -1, 'smiling'] = 0
+    y_train = dataset['smiling']
+    img_name1 = dataset['img_name']
+
+    x_train = []
+    for name in img_name1:
+        img_path = './Datasets/celeba/img/' + name  # get path based on image name
+        img = loadImage(img_path)
+        x_train.append(img)  # add pic to x_train
+
+    # Load test data
+    data_test = pd.read_csv('./Datasets/celeba_test/labels.csv',
+                            sep="\t")  # read csv file
+    data_test.loc[data_test['smiling'] == -1, 'smiling'] = 0
+    y_test = data_test['smiling']
+    img_name2 = data_test['img_name']
+
+    x_test = []
+    for name in img_name2:
+        img_path = './Datasets/celeba_test/img/' + name  # get path based on image name
+        img = loadImage(img_path)
+        x_test.append(img)  # add pic to x_train
+
+    sc = StandardScaler()
+    x_train = sc.fit_transform(x_train)
+    x_test = sc.transform(x_test)
+
+    y_pred1 = logRegrNEWRegrPredict(x_train, y_train, x_test)
+
+    k_test = getK(y_pred1)
+    y_pred2 = logRegrNEWRegrPredict(x_train, y_train, x_train)
+    k_train = getK(y_pred2)
+
+    end = timer()
+    print('Computation time: ', end - start)
+
+    print('Accuracy on train set: ' + str(accuracy_score(y_train, k_train)))
+    print('Accuracy on test set: ' + str(accuracy_score(y_test, k_test)))
+    print('Confusion matrix: ', '\n', confusion_matrix(y_test, k_test))
+    print('MSE: ' + str(getMSE(k_test, y_test)))
+    print('Classification report: ', '\n',
+          classification_report(y_test, k_test))  # text report showing the main classification metrics
+
